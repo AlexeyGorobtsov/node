4604962c7addd058da8fb0e062adf911dbff571a
gorobtsovalexey <lexa2987@mail.ru>
2022-01-29 15:51:36 +0300

    service 1:/ping

--- app/index.js
+++ app/index.js
@@ -100,17 +100,17 @@ const unifiedServer = function (req, res) {
 // Define the handlers
 const handlers = {};
 
-handlers.sample = function (data, callback) {
-    // Callback a http status code, and a payload object
-    callback(406, {'name': 'sample handler'});
-}
 
 // Not found handler
 handlers.notFound = function (data, callback) {
     callback(404);
 }
 
+handlers.ping = function (data, callback) {
+    callback(200);
+}
+
 // Define a request router
 const router = {
-    'samples': handlers.sample
+    'ping': handlers.ping
 }
