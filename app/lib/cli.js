22214630b472aec9d2079873a9ea6cff51310d59
aleksejgorobcov <lexa2987@mail.ru>
2023-04-17 23:26:05 +0300

    Command 1: Man/Help

--- app/lib/cli.js
+++ app/lib/cli.js
@@ -8,7 +8,10 @@ const util = require('util');
 const debug = util.debuglog('cli');
 const events = require('events');
 const config = require("../config");
-class  _events extends events{}
+
+class _events extends events {
+}
+
 const e = new _events();
 
 // Instantiate the CLI module object
@@ -61,55 +64,132 @@ e.on('more log info', function (str) {
 cli.responders = {};
 
 // Help / Man
-cli.responders.help = function (){
-    console.log('You asked for help');
+cli.responders.help = function () {
+    const commands = {
+        'exit': 'Kill the CLI (and the rest of the application)',
+        'man': 'Show this help page',
+        'help': 'Alias of "man" command',
+        'stats': 'Get statistics on the underlying operating system and resource utilization',
+        'list users': 'Show a list of all the registered (undeleted) users in the system ',
+        'more user info --{userId}': 'Show details of a specific user',
+        'list checks --up --down': 'Show a list of all the active checks in the system, including their state. The "--up" and the "--down" flags are both optional',
+        'more check info --{checkId}': 'Show details of a specified check',
+        'list logs': 'Show a list of all the log files available to be read (compressed and uncompressed)',
+        'more log info --{fileName}': 'Show details of a specified log file',
+    }
+
+    // Show a header for the help page that is as wide as the screen
+    cli.horizontalLine();
+    cli.centered('CLI MANUAL');
+    cli.horizontalLine();
+    cli.verticalSpace(2);
+
+    // Show each command, followed by its explanation, in white and yellow respectively
+    for (let key in commands) {
+        if (commands.hasOwnProperty(key)) {
+            const value = commands[key];
+            let line = '\x1b[33m' + key + '\x1b[0m';
+            const padding = 60 - line.length;
+            for (let i = 0; i < padding; i++) {
+                line += ' '
+            }
+            line += value;
+            console.log(line);
+            cli.verticalSpace();
+        }
+    }
+    cli.verticalSpace(1);
+
+    // End with another horizontalLine
+    cli.horizontalLine();
 };
 
+// Create a vertical space
+cli.verticalSpace = function (lines) {
+    lines = typeof lines === 'number' && lines > 0 ? lines : 1;
+    for (let i = 0; i < lines; i++) {
+        console.log('')
+    }
+}
+
+// Create a horizontal line across the screen
+cli.horizontalLine = function () {
+    // Get the available screen size
+    const width = process.stdout.columns;
+    let line = '';
+
+    for (let i = 0; i < width; i++) {
+        line += '-';
+    }
+
+    console.log(line);
+}
+
+// Create centered text on the screen
+cli.centered = function (str) {
+    str = typeof (str) === 'string' && str.trim().length > 0 ? str.trim() : '';
+
+    // Get the available screen size
+    const width = process.stdout.columns;
+
+    // Calculate the left padding there should be
+    const leftPadding = Math.floor((width - str.length) / 2);
+
+    // Put in left padded spaces before the string itself
+    let line = '';
+    for (let i = 0; i < leftPadding; i++) {
+        line += ' ';
+    }
+
+    line += str;
+    console.log(line)
+}
+
 // Exit
-cli.responders.exit = function (){
+cli.responders.exit = function () {
     process.exit(0)
 };
 
 // Stats
-cli.responders.stats = function (){
+cli.responders.stats = function () {
     console.log('You asked for stats');
 };
 
 // List users
-cli.responders.listUsers = function (){
+cli.responders.listUsers = function () {
     console.log('You asked to list users');
 };
 
 // More user info
-cli.responders.moreUserInfo = function (str){
+cli.responders.moreUserInfo = function (str) {
     console.log('You asked for more user info', str);
 };
 
 // List checks
-cli.responders.listChecks = function (str){
+cli.responders.listChecks = function (str) {
     console.log('You asked to list checks', str);
 };
 
 // More check info
-cli.responders.moreCheckInfo = function (str){
+cli.responders.moreCheckInfo = function (str) {
     console.log('You asked for more check info', str);
 };
 
 // List logs
-cli.responders.listLogs = function (){
+cli.responders.listLogs = function () {
     console.log('You asked to list logs');
 };
 
 // More logs info
-cli.responders.moreLogsInfo = function (str){
+cli.responders.moreLogsInfo = function (str) {
     console.log('You asked for more logs info', str);
 };
 
 // Input processor
 cli.processInput = function (str) {
-    str = typeof(str) === 'string' && str.trim().length > 0 ? str.trim() : false;
+    str = typeof (str) === 'string' && str.trim().length > 0 ? str.trim() : false;
     // Only process the input if the user actually wrote something. Otherwise ignore
-    if(str) {
+    if (str) {
         // Codify the unique strings that identify the unique questions allowed to be asked
         const uniqueInputs = [
             'man',
@@ -128,16 +208,16 @@ cli.processInput = function (str) {
         let matchFound = false;
         const counter = 0;
         uniqueInputs.some((input) => {
-            if(str.toLowerCase().indexOf(input) > -1){
-                 matchFound = true;
-                 // Emit an event matching the unique input, and include the full string given
+            if (str.toLowerCase().indexOf(input) > -1) {
+                matchFound = true;
+                // Emit an event matching the unique input, and include the full string given
                 e.emit(input, str);
                 return true;
             }
         })
 
         // If no match is found, tell the user to tyr again
-        if(!matchFound) {
+        if (!matchFound) {
             console.log('Sorry, try again');
         }
 
