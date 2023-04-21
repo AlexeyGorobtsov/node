54cec232f945cbf5ecdd21c2c6d1bbdedd3783b3
aleksejgorobcov <lexa2987@mail.ru>
2023-04-21 22:47:28 +0300

    Command 3: list users

--- app/lib/cli.js
+++ app/lib/cli.js
@@ -15,6 +15,7 @@ class _events extends events {
 const e = new _events();
 const os =  require('os');
 const v8 = require('v8');
+const _data = require('./data');
 
 // Instantiate the CLI module object
 const cli = {}
@@ -195,7 +196,22 @@ cli.responders.stats = function () {
 
 // List users
 cli.responders.listUsers = function () {
-    console.log('You asked to list users');
+    _data.list('users', function (err, userIds) {
+        if(!err && userIds && userIds.length > 0) {
+            cli.verticalSpace();
+            userIds.forEach(function (userId) {
+                _data.read('users', userId, function (err, userData) {
+                    if(!err && userData) {
+                        let line = 'Name: '+userData.firstName+' '+userData.lastName+' Phone: '+ userData.phone+ ' Checks: ';
+                        const numberOfChecks = typeof userData.checks === 'object' && userData.checks instanceof Array && userData.checks.length > 0  ? userData.checks.length : 0;
+                        line += numberOfChecks;
+                        console.log(line);
+                        cli.verticalSpace();
+                    }
+                })
+            })
+        }
+    })
 };
 
 // More user info
